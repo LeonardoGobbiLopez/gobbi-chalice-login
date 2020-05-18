5adecbd2f2993b39dc42fac5ad2040b1808bed65
leonardogobbilopez <leonardoglopez@gmail.com>
2020-05-18 20:14:40 -0300

    first commit

---
+++ src/amplify-config.js
@@ -0,0 +1,7 @@
+const amplifyConfig = {
+  "aws_cognito_region": "us-east-1",
+  "aws_user_pools_id": "us-east-1_B7ayyqP6H",
+  "aws_user_pools_web_client_id": "72p11g45u8f7assq2vti7mip1d"
+};
+
+export default amplifyConfig;
